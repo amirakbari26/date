70d6cec8c9b0d87a8b70db5d87ffaea6c557be6a
amirakbari26 <amakbari26@gmail.com>
2022-04-23 23:45:21 +0430

    complate remaining work days section

--- index.rb
+++ index.rb
@@ -1,6 +1,8 @@
+require 'parsi-date'
 require 'rspec'
 require 'date'
-require 'parsi-date'
+
+
 
 #current day information
 
@@ -22,7 +24,6 @@ if tc == "2"
 end
 
 
-
 #all remaining days
 
 puts "please enter start date (e.g 2020-04-12)"
@@ -36,14 +37,60 @@ ed = Date.parse(de)
 
 def day_counter(sd , ed)
     c = sd.upto(ed).select { |date|  date }.count
-    puts "all remaining days; #{c}"
+    puts "all remaining days: #{c}"
 
 end
 
 day_counter sd ,ed
 
 
-#remaining work days
 
-c = sd.upto(ed).select{ |d| d.saturday? || d.sunday? || d.monday? || d.tuesday? || d.wednesday? }.count
-puts "remaining work days : #{c}"
+#remaining work days
+dayarray = Array[["01","02","03","04","05","06","07","08","09","10","11","12","13"],["14","15"],["22"],["29"]]
+montharray = Array["فروردین","خرداد","بهمن","اسفند"]
+
+arr = ds.split('-',3)
+parsisd = Time.new(arr.at(0), arr.at(1), arr.at(2)).to_parsi
+
+count = 0
+while sd <= ed do
+    nday= sd.strftime("%A")
+    nm = parsisd.strftime("%B")
+    numberday = parsisd.strftime("%d")
+
+    if nday != "Thursday" && nday != "Friday"
+        count += 1
+
+    end
+
+
+
+    if montharray.include?(nm)
+
+        if  nm == "فروردین" && dayarray.at(0).include?(numberday) && nday != "Thursday" && nday != "Friday"
+
+            count -= 1
+        end
+
+        if  nm == "خرداد" && dayarray.at(1).include?(numberday) && nday != "Thursday" && nday != "Friday"
+
+         count -= 1
+        end
+
+        if  nm == "بهمن" && dayarray.at(2).include?(numberday) && nday != "Thursday" && nday != "Friday"
+
+           count -= 1
+        end
+
+        if  nm == "اسفند" && dayarray.at(3).include?(numberday) && nday != "Thursday" && nday != "Friday"
+
+            count -= 1
+        end
+
+    end
+
+
+    sd = sd + 1
+    parsisd = parsisd + 1
+end
+puts "remaining work days: #{count}"
